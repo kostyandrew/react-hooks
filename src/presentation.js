9528c72106e2d15020b3949c4ac45dc2a09710ad
Andrii Kostetskyi <kostyandrew@gmail.com>
2019-05-14 01:41:13 +0300

    initial

--- src/presentation.js
+++ src/presentation.js
@@ -1,95 +1,237 @@
 // Import React
-import React from 'react';
-
+import React from "react";
 // Import Spectacle Core tags
-import {
-  BlockQuote,
-  Cite,
-  Deck,
-  Heading,
-  ListItem,
-  List,
-  Quote,
-  Slide,
-  Text,
-} from 'spectacle';
+import { Deck, Heading, Slide, Text, Image, Appear } from "spectacle";
 
 // Import theme
-import createTheme from 'spectacle/lib/themes/default';
+import createTheme from "spectacle/lib/themes/default";
 
 // Require CSS
-require('normalize.css');
+import "normalize.css";
+import "./style.css";
 
 const theme = createTheme(
   {
-    primary: 'white',
-    secondary: '#1F2022',
-    tertiary: '#03A9FC',
-    quaternary: '#CECECE',
+    primary: "white",
+    secondary: "#1F2022",
+    heading: "#7b0046",
+    tertiary: "#03A9FC",
+    quaternary: "grey"
   },
   {
-    primary: 'Montserrat',
-    secondary: 'Helvetica',
+    primary: "Montserrat",
+    secondary: "Helvetica"
   }
 );
+
+/* eslint-disable-next-line import/no-webpack-loader-syntax */
+const kottans = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 120 120"><image opacity="0.2" xlink:href="${require("!url-loader!./resources/kottans.png")}" x="10" y="10" height="100" width="100"/></svg>`;
+const stroke = `<svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100"><line x1="20" y1="20" x2="80" y2="80" stroke="blue" stroke-width="10"/><line x1="90" y1="10" x2="10" y2="90" stroke="red" stroke-width="10"/></svg>`;
+
+const transitionDuration = 500;
+
+const Iframe = ({ id, module, highlights, ...props }) => {
+  const [loaded, setLoaded] = React.useState(false);
+  const [afterTransition, setAfterTransition] = React.useState(false);
+  React.useEffect(() => {
+    const timeout = window.setTimeout(
+      () => setAfterTransition(true),
+      transitionDuration
+    );
+    return () => window.clearTimeout(timeout);
+  }, []);
+  return (
+    <div
+      className={`iframe-wrapper iframe-wrapper_${
+        loaded ? "loaded" : "loading"
+      }`}
+    >
+      {afterTransition && (
+        <iframe
+          {...props}
+          src={`https://codesandbox.io/embed/${id}?fontsize=16&codemirror=1&view=editor&hidenavigation=1${
+            module ? `&module=${module}` : ""
+          }${highlights ? `&highlights=${highlights}` : ""}`}
+          title={props.title}
+          onLoad={() => setLoaded(true)}
+          sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
+        />
+      )}
+    </div>
+  );
+};
 
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck
-        transition={['zoom', 'slide']}
-        transitionDuration={500}
-        theme={theme}
-      >
-        <Slide transition={['zoom']} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+      <Deck progress="pacman" theme={theme}>
+        <Slide
+          bgColor="primary"
+          style={{
+            backgroundImage: `url('data:image/svg+xml;utf8,${kottans}')`,
+            backgroundSize: "10vw"
+          }}
+        >
+          <Heading
+            size={1}
+            caps
+            textColor="heading"
+            style={{ fontSize: "7vh" }}
+          >
+            <Image
+              display="inline-block"
+              src={require("./resources/logo.svg")}
+              style={{
+                verticalAlign: "bottom",
+                margin: 0,
+                height: "1em",
+                animation: "rotate 3.5s infinite linear"
+              }}
+            />{" "}
+            React Continued
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+          <Heading
+            size={2}
+            style={{ fontSize: "6vh", color: "#555", margin: "3vh 0 5vh 0" }}
+          >
+            Part 3
+          </Heading>
+          <Text
+            margin="10px 0 0"
+            style={{ fontSize: "6vh" }}
+            textColor="secondary"
+            size={3}
+            bold
+          >
+            Hooks, Refs, Context
+            <br /> and Witch of Portal
           </Text>
         </Slide>
-        <Slide transition={['fade']} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>
-            Typography
-          </Heading>
-          <Heading size={1} textColor="secondary">
-            Heading 1
-          </Heading>
-          <Heading size={2} textColor="secondary">
-            Heading 2
-          </Heading>
-          <Heading size={3} textColor="secondary">
-            Heading 3
-          </Heading>
-          <Heading size={4} textColor="secondary">
-            Heading 4
-          </Heading>
-          <Heading size={5} textColor="secondary">
-            Heading 5
-          </Heading>
-          <Text size={6} textColor="secondary">
-            Standard text
-          </Text>
+        <Slide bgColor="pink">
+          <div style={{ display: "flex" }} style={{ fontSize: "7vh" }}>
+            <Image
+              style={{ height: "20vh", borderRadius: "20% 40% 0% 15%" }}
+              src={require("./resources/i.jpg")}
+            />
+            <div
+              style={{
+                flexGrow: 1,
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center"
+              }}
+            >
+              <Heading
+                textColor="white"
+                style={{ fontSize: "1em", marginBottom: "0.5em" }}
+              >
+                Andrii Kostetskyi
+              </Heading>
+              <Text textColor="white">
+                Fullstack Developer in <i>ASD</i>
+              </Text>
+            </div>
+          </div>
         </Slide>
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>
-            Standard List
+        <Slide
+          bgColor="black"
+          style={{
+            backgroundImage: `url('data:image/svg+xml;utf8,${stroke}')`,
+            backgroundSize: "100vw 100vh"
+          }}
+        >
+          <Heading
+            style={{ fontSize: "10vmin", textShadow: "0 0 8px white" }}
+            textColor="white"
+          >
+            class extend React.Component
           </Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
         </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
+        <Slide>
+          <SlideHeading textColor="tertiary" style={{ fontSize: 70 }}>
+            Use Hooks
+          </SlideHeading>
+          <Heading size={2} style={{ fontSize: 40 }}>
+            React 16.8
+          </Heading>
+        </Slide>
+        <Slide bgColor="red">
+          <SlideHeading textColor="white">Use State</SlideHeading>
+
+          <Iframe
+            highlights="4"
+            id="67xl58mw3"
+            module="%2Fsrc%2FCounterButton.jsx"
+            title="Use state"
+          />
+        </Slide>
+        <Slide bgColor="orange">
+          <SlideHeading textColor="secondary">Use Effect</SlideHeading>
+          <Iframe
+            highlights="6"
+            id="6vz86wpzzr"
+            module="%2Fsrc%2FTimer.jsx"
+            title="Use Effect"
+          />
+        </Slide>
+        <Slide bgColor="yellow">
+          <SlideHeading textColor="secondary">Life Cycles</SlideHeading>
+          <Iframe
+            highlights="5,9,13"
+            id="k0qwkrzjr"
+            module="%2Fsrc%2FCycles.jsx"
+            title="Life Cycles"
+          />
+        </Slide>
+        <Slide bgColor="green">
+          <SlideHeading textColor="white">Use Ref</SlideHeading>
+          <Iframe
+            highlights="5,8"
+            id="y392l8j18z"
+            module="%2Fsrc%2FJquery.jsx"
+            title="Use Ref"
+          />
+        </Slide>
+        <Slide bgColor="cyan">
+          <SlideHeading textColor="secondary">Use Memo</SlideHeading>
+          <Iframe
+            highlights="4,14"
+            id="728zzr1rkj"
+            module="%2Fsrc%2FMemo.jsx"
+            title="Use Memo"
+          />
+        </Slide>
+        <Slide bgColor="blue">
+          <SlideHeading textColor="white">Context</SlideHeading>
+          <Iframe
+            highlights="3,10"
+            id="k2om741zp7"
+            module="%2Fsrc%2FTheme.jsx"
+            title="Context"
+          />
+        </Slide>
+        <Slide bgColor="purple">
+          <SlideHeading textColor="white">Portal</SlideHeading>
+          <Iframe
+            highlights="8"
+            id="615rlr6qnw"
+            module="%2Fsrc%2FModal.jsx"
+            title="Portal"
+          />
         </Slide>
       </Deck>
     );
   }
 }
+
+const SlideHeading = props => (
+  <Heading
+    {...props}
+    size={2}
+    style={{
+      ...props.style,
+      fontSize: "10vh",
+      textShadow: "0 0 8px currentColor"
+    }}
+  />
+);
